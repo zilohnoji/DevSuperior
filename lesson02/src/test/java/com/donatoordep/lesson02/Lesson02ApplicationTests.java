ed82638b6e52ab3aaafe5cd942ca3aa6d97e460a
Pedro Donato <emagdonatdonatoordep@gmail.com>
2023-06-24 17:52:44 -0300

    Lesson 02

---
+++ lesson02/src/test/java/com/donatoordep/lesson02/Lesson02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donatoordep.lesson02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
