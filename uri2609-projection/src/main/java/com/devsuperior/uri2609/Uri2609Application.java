9b0ce42c40e1892c37496668f116596a350a4ac5
Pedro Donato <emagdonatdonatoordep@gmail.com>
2023-06-29 09:59:19 -0300

    New projection

---
+++ uri2609-projection/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -0,0 +1,25 @@
+package com.devsuperior.uri2609;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.devsuperior.uri2609.repositories.CategoryRepository;
+
+@SpringBootApplication
+public class Uri2609Application implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uri2609Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.getSum().forEach(x -> System.out.println(x.getName() + ": " + x.getSum()));
+        repository.getSumDto().forEach(x -> System.out.println(x.getName() + ":" + x.getSum()));
+    }
+}
