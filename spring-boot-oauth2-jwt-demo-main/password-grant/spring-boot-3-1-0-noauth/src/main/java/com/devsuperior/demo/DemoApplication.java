f0a60614bedf3dadfcd13bbd7afaaa4ebac376ea
Pedro Donato <emagdonatdonatoordep@gmail.com>
2023-07-03 17:54:16 -0300

    Controll access per route

--- spring-boot-oauth2-jwt-demo-main/password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ spring-boot-oauth2-jwt-demo-main/password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -7,18 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println(passwordEncoder.encode("123456"));
-        System.out.println(passwordEncoder.matches("123456", "$2a$10$HuW6wIR2PUIV0G0eRR/Sy..Jg1cRO5toHd8a06/u5ROHq9.hogaoO"));
     }
 }
